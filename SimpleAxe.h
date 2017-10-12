c3eaced506d987af503fce9b3aafdc0a7ce41c9e
KBL13 <kbl13b@my.fsu.edu>
2017-10-12 00:05:07 -0400

    Created the header file for the simple axe weapon class.
    Will implement attributes in next commit for the cpp file of simple axe.

---
+++ SimpleAxe.h
@@ -0,0 +1,18 @@
+#include <string>
+#include "Weapon.h"
+
+#ifndef SIMPLEAXE_H
+#define SIMPLEAXE_H
+
+class SimpleAxe : public Weapon {
+public:
+
+    SimpleAxe() : Weapon("Simple Axe", 60.0) {
+    }
+    virtual ~SimpleAxe() {};
+    virtual double hit(double armor);
+
+};
+
+#endif
+
