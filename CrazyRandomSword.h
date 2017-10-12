34ee33e613e3a6cc44b9e96b182db6d582418bdb
KBL13 <kbl13b@my.fsu.edu>
2017-10-12 00:22:59 -0400

    Implemented the CrazyRandomSword weapon header file.
    Detailed specification of hitpoints being a random int between 10 & 100.

---
+++ CrazyRandomSword.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include "Weapon.h"
+
+#ifndef CRAZYRANDOMSWORD_H
+#define CRAZYRANDOMSWORD_H
+
+class CrazyRandomSword : public Weapon {
+public:
+	int hitpointsTemp;
+	hitpointsTemp = rand() % 100 + 10;
+    CrazyRandomSword() : Weapon("Crazy Random Sword", hitpointsTemp) {
+    }
+    virtual ~CrazyRandomSword() {};
+    virtual double hit(double armor);
+
+};
+
+#endif
